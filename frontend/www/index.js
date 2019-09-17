1f18f3d16fc7a897da7abf74748dbf8ba7467a31
ajuvercr <arthur.vercruysse@ugent.be>
2019-09-17 18:27:44 +0200

    bump

--- frontend/www/index.js
+++ frontend/www/index.js
@@ -1,5 +1,4 @@
 import { Game } from "planetwars";
-import { memory } from "planetwars/plantwars_bg"
 import { Shader } from "./webgl/shader"
 
 import { main } from './index.ts'
@@ -14,19 +13,3 @@ fetch(game_location)
     .then((response) => {
         main(Game.new(response));
     }).catch(console.error);
-
-
-// const g = Game.new("");
-
-// const p1 = g.locations();
-// const s1 = g.location_count();
-// console.log(p1, s1);
-// const a1 = new Float64Array(memory.buffer, p1, s1 * 3);
-// console.log(a1);
-
-// g.add_location(0.5, 1.2, 3.14);
-
-// const p2 = g.locations();
-// const s2 = g.location_count();
-// const a2 = new Float64Array(memory.buffer, p2, s2 * 3);
-// console.log(a2);
--- frontend/www/index.ts
+++ frontend/www/index.ts
@@ -1,5 +1,15 @@
 import { Game } from "planetwars";
+import { memory } from "planetwars/plantwars_bg";
+
+const CANVAS = <HTMLCanvasElement>document.getElementById("c");
+
+
+function create_array(ptr: number, size: number): Float64Array {
+    return new Float64Array(memory.buffer, ptr, size);
+}
 
 export function main(game: Game) {
     console.log(game.turn_count());
+
+    console.log(create_array(game.get_viewbox(), 4));
 }
