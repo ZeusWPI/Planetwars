7165e7339f2778aa9a9176ba790b4bdfbe7f8de5
ajuvercr <arthur.vercruysse@ugent.be>
2019-10-20 19:32:05 +0200

    BETTER GRIDS

--- mapbuilder/src/index.ts
+++ mapbuilder/src/index.ts
@@ -1,4 +1,25 @@
 
+let gridlineEls:any;
+let gridlineNeedsUpdate = true;
+
+function get_line_widths(): string[][] {
+    if (gridlineNeedsUpdate) {
+        gridlineEls = document.getElementsByClassName("gridline");
+        gridlineNeedsUpdate = false;
+    }
+    const out: string[][] = [[], [], []];
+    for(let el of <any[]>gridlineEls) {
+        let i = 0;
+        for( let c of el.getElementsByTagName('input')) {
+            out[i].push(
+                c.value
+            );
+            i += 1;
+        }
+    }
+    return out;
+}
+
 function main() {
     const canvas = <HTMLCanvasElement>document.getElementById("canvas");
     const ctx = canvas.getContext('2d');
@@ -9,44 +30,52 @@ function main() {
     canvas.width = width;
     canvas.height = height;
 
-    ctx.translate(width / 2, height / 2);
+    function draw() {
+        ctx.clearRect(0, 0, width, height);
 
+        ctx.save();
 
-    ctx.strokeStyle = "#333"
-    draw_grid(
-        ctx, [width, 100, 20], [width / 2, height / 2], [width, height],
-        4, 0.30
-    );
-    ctx.fillStyle = "#ff5f00";
+        ctx.translate(width / 2, height / 2);
 
-    ctx.fillRect(0, 0, 100, 100);
+        ctx.strokeStyle = "#333";
+        draw_grid(ctx, get_line_widths(), [width / 2, height / 2], [width, height]);
+        ctx.fillStyle = "#ff5f00";
+
+        ctx.fillRect(0, 0, 100, 100);
+
+        ctx.restore();
+        requestAnimationFrame(draw);
+    }
 
     console.log("hallo");
+    draw();
+
 }
 
 function draw_grid(
     ctx: CanvasRenderingContext2D,
-    lines: number[],    // number of pixels between lines, with smaller gettings lines
+    lines: string[][],    // number of pixels between lines, with smaller gettings lines
     transform: [number, number],
-    size: [number, number],
-    init_width = 2, factor = 0.5) {
+    size: [number, number]) {
 
     ctx.save();
 
     ctx.translate(-transform[0], -transform[1]);
 
-
-    for (let delta of lines) {
-        ctx.lineWidth = init_width;
+    for (let i = lines[0].length - 1; i >= 0 ; i -= 1) {
+        ctx.lineWidth = parseInt(lines[0][i]);
+        const delta = parseInt(lines[1][i]);
+        ctx.strokeStyle = "transparent";
+        ctx.strokeStyle = lines[2][i];
 
         let xoff = transform[0] % delta;
         let yoff = transform[1] % delta;
 
         ctx.beginPath();
 
-        for (let i = xoff; i < size[0]; i += delta) {
-            ctx.moveTo(i, 0);
-            ctx.lineTo(i, size[1]);
+        for (let j = xoff; j < size[0]; j += delta) {
+            ctx.moveTo(j, 0);
+            ctx.lineTo(j, size[1]);
         }
         for (let j = yoff; j < size[1]; j += delta) {
             ctx.moveTo(0, j);
@@ -54,8 +83,6 @@ function draw_grid(
         }
 
         ctx.stroke();
-
-        init_width *= factor;
     }
 
     ctx.restore();
