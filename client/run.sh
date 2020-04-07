c0f31e61df257a1a796b21e2ca83d12b38730b90
redfast00 <redfast00@gmail.com>
2020-04-07 23:57:41 +0200

    Make python3 explicit for non-Arch based systems (as if those even exist)

--- client/run.sh
+++ client/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Using token $1"
-python runner.py -n simple.py --host ${HOST:-localhost} -p 9142 -i $1 python simple.py
+python3 runner.py -n simple.py --host ${HOST:-localhost} -p 9142 -i $1 python3 simple.py
