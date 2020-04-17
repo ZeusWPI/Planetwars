bebc10a81f9f2cbc14594fd0618e62a4a9f1667c
ajuvercr <arthur.vercruysse@outlook.com>
2020-04-17 17:03:12 +0200

    make things beautiful

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -284,10 +284,6 @@ impl Game {
         }).collect();
     }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e5af4f4... too late commit lol
     fn update_ship_counts(&mut self) {
         self.ship_counts = self.states[self.turn].expeditions.iter().map(|s| {
             s.ship_count as usize
